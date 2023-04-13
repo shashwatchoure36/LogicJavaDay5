7295e42fbda38d294e744df7f39312af699575a3
shashwatchoure365 <shashwatchoure36@gmail.com>
2023-04-13 13:18:35 +0530

    [Shashwat] Add : This Script use to Create Reverse number program

---
+++ src/com/logicjava/ReverseNumber.java
@@ -0,0 +1,23 @@
+package com.logicjava;
+import java.util.Scanner;
+
+public class ReverseNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	       int m, n, sum = 0;
+	        Scanner s = new Scanner(System.in);
+	        System.out.print("Enter the number:");
+	        m = s.nextInt();
+	        while(m > 0)
+	        {
+	            n = m % 10;
+	            sum = sum * 10 + n;
+	            m = m / 10;
+	        }
+	        System.out.println("Reverse of a Number is "+sum);
+
+	}
+
+}
